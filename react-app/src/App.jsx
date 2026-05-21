88776f0fd1f59c3bc7ebc2934c226a105c21fd26
The Great White Rabbit <gribomen@mail.ru>
2026-05-21 15:47:10 +0300

    Поверстал ещё один компонент JournalItem

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,17 +1,24 @@
 //import React from 'react';
 
 import './App.css';
+import Button from './components/Button/Button';
+import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
 
   //return React.createElement('div', {}, React.createElement(
   //));
 
+
   return (
+    //Вот это react.fragment нужен для обертки содержимого компонента.
     <>
       <h1>Заголовок</h1>
       <p>Какой-то текст</p>
+      <Button />
+      <JournalItem />
     </>
+    //
   );
 }
 
