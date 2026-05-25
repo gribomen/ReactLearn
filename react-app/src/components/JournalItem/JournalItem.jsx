4879d74738e1d82c915535c3a0544ad771b0f411
The Great White Rabbit <gribomen@mail.ru>
2026-05-25 23:42:53 +0300

    Интернациализировали дату

--- react-app/src/components/JournalItem/JournalItem.jsx
+++ react-app/src/components/JournalItem/JournalItem.jsx
@@ -1,10 +1,13 @@
 import './JournalItem.css';
 function JournalItem({ title, date, text }) {
+    const formatedDate = new Intl.DateTimeFormat('ru-RU', { day: '2-digit', year: 'numeric', month: '2-digit' })
+        .format(date)
+        .replace(/\./g, '/');
     return (
         <div className="journal-item">
             <h2 className="journal-item__header">{title}</h2>
             <h2 className="journal-item__body">
-                <div className="journal-item__date">{date.toString()}</div>
+                <div className="journal-item__date">{formatedDate}</div>
                 <div className="journal-item__text">{text}</div>
             </h2>
         </div>
