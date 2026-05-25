70d8e9e53c1a7e076908a565d6dbdd2e3ee96a4e
The Great White Rabbit <gribomen@mail.ru>
2026-05-26 00:21:06 +0300

    Немного позанимался композицией узнал как упаковывать компоненты внутри других компонентов

--- react-app/src/components/JournalItem/JournalItem.jsx
+++ react-app/src/components/JournalItem/JournalItem.jsx
@@ -1,13 +1,13 @@
 import './JournalItem.css';
 function JournalItem({ title, date, text }) {
     return (
-        <div className="journal-item">
+        <>
             <h2 className="journal-item__header">{title}</h2>
             <h2 className="journal-item__body">
                 <div className="journal-item__date">{date.toString()}</div>
                 <div className="journal-item__text">{text}</div>
             </h2>
-        </div>
+        </>
     );
 }
 
