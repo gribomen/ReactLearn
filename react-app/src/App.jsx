e48e35ce656db9e0109ec20d211f6ae8e913d532
The Great White Rabbit <gribomen@mail.ru>
2026-06-02 17:02:46 +0300

    Потрогал контролируемые компоненты.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,12 @@
-//import React from 'react';
-
 import './App.css';
-import Button from './components/Button/Button';
-import CardButton from './components/CardButton/CardButton';
+import LeftPanel from './components/layouts/LeftPanel/LeftPanel';
+import Body from './components/layouts/Body/Body';
 import JournalItem from './components/JournalItem/JournalItem';
-
+import CardButton from './components/CardButton/CardButton';
+import Header from './components/Header/Header';
+import JournalList from './components/JournalList/JournalList';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
+import { useState } from 'react';
 function App() {
 
   const data = [
@@ -20,30 +22,40 @@ function App() {
     }
   ];
 
+  const [inputData, setInputData] = useState('');
+  const inputChange = (event) => {
+    setInputData(event.target.value);
+    console.log(inputData);
+  };
+
   return (
-    //Вот это react.fragment нужен для обертки содержимого компонента.
-    <>
-      <h1>Заголовок</h1>
-      <p>Какой-то текст</p>
-      <Button />
-      <CardButton>
-        Новое воспоминание+
-      </CardButton>
-      <CardButton>
-        <JournalItem
-          title={data[0].title}
-          text={data[0].text}
-          date={data[0].date}
-        />
-      </CardButton>
-      <CardButton>
-        <JournalItem
-          title={data[1].title}
-          text={data[1].text}
-          date={data[1].date}
-        />
-      </CardButton>
-    </>
+
+    <div className='app'>
+      <LeftPanel>
+        <Header />
+        <JournalAddButton />
+        <JournalList>
+          <CardButton>
+            <JournalItem
+              title={data[0].title}
+              text={data[0].text}
+              date={data[0].date}
+            />
+          </CardButton>
+          <CardButton>
+            <JournalItem
+              title={data[1].title}
+              text={data[1].text}
+              date={data[1].date}
+            />
+          </CardButton>
+        </JournalList>
+      </LeftPanel>
+      <Body>
+        <input type="text" value={inputData} onChange={inputChange} />
+      </Body>
+
+    </div>
     //
   );
 }
--- react-app/src/components/JournalItem/JournalItem.jsx
+++ react-app/src/components/JournalItem/JournalItem.jsx
@@ -4,7 +4,7 @@ function JournalItem({ title, date, text }) {
         <>
             <h2 className="journal-item__header">{title}</h2>
             <h2 className="journal-item__body">
-                <div className="journal-item__date">{date.toString()}</div>
+                <div className="journal-item__date">{Intl.DateTimeFormat('ru-RU').format(date)}</div>
                 <div className="journal-item__text">{text}</div>
             </h2>
         </>
