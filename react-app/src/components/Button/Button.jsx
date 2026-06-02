7765879b54f74009ed57cd77319fd5e760b91721
The Great White Rabbit <gribomen@mail.ru>
2026-06-02 22:34:27 +0300

    Изучил stateful и stateless компоненты.

--- react-app/src/components/Button/Button.jsx
+++ react-app/src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import './Button.css';
-function Button({ text }) {
+function Button({ text, onClick }) {
 	return (
-		<button className='button accent'>{text}</button>
+		<button className='button accent' onClick={onClick}>{text}</button>
 	);
 }
 
