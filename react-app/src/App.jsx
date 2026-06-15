70d57ec2b543efa17d4bd9a83862e550857456fe
The Great White Rabbit <gribomen@mail.ru>
2026-06-15 21:49:12 +0300

    Добавил условие в журнал лист, сделал как тупой компонент

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import './App.css';
-import JournalItem from './components/JournalItem/JournalItem';
-import CardButton from './components/CardButton/CardButton';
 import Header from './components/Header/Header';
 import JournalList from './components/JournalList/JournalList';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
@@ -10,18 +8,18 @@ import LeftPanel from './components/layouts/LeftPanel/LeftPanel';
 import Body from './components/layouts/Body/Body';
 
 const INITIAL_ITEM = [
-  {
-    id: 1,
-    title: 'Подготовка к обновлению курсов',
-    date: new Date(),
-    text: 'Горные походы открывают удивительные природные ландшафты'
-  },
-  {
-    id: 2,
-    title: 'Поход в годы',
-    date: new Date(),
-    text: 'Думал, что очень много времени'
-  }
+  //{
+  //  id: 1,
+  //  title: 'Подготовка к обновлению курсов',
+  //  date: new Date(),
+  //  text: 'Горные походы открывают удивительные природные ландшафты'
+  //},
+  //{
+  //  id: 2,
+  //  title: 'Поход в годы',
+  //  date: new Date(),
+  //  text: 'Думал, что очень много времени'
+  //}
 ];
 
 function App() {
@@ -36,28 +34,12 @@ function App() {
     }]);
   };
 
-  const sortItems = (a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    }
-    return - 1;
-  };
-
   return (
     <div className='app'>
       <LeftPanel>
         <Header />
         <JournalAddButton />
-        <JournalList>
-          {journalItems.sort(sortItems).map(journalItem =>
-            <CardButton key={journalItem.id}>
-              <JournalItem
-                title={journalItem.title}
-                text={journalItem.text}
-                date={journalItem.date}
-              />
-            </CardButton>)}
-        </JournalList>
+        <JournalList items={journalItems} />
       </LeftPanel>
       <Body>
         <JournalForm onSubmit={addItem} />
