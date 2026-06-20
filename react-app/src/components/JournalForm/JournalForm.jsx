158a8bdfad38578166e2b208cf3e465a880d34a1
The Great White Rabbit <gribomen@mail.ru>
2026-06-21 00:03:13 +0300

    Обработал работу с формой

--- react-app/src/components/JournalForm/JournalForm.jsx
+++ react-app/src/components/JournalForm/JournalForm.jsx
@@ -23,29 +23,33 @@ function JournalForm({ onSubmit }) {
     useEffect(() => {
         if (isFormReadyToSubmit) {
             onSubmit(values);
+            dispatchForm({ type: 'CLEAR' });
         }
     }, [isFormReadyToSubmit]);
 
+
+    const onChange = (e) => {
+        dispatchForm({ type: 'SET_VALUE', payload: { [e.target.name]: e.target.value } });
+    };
     const addJournalItem = (e) => {
         e.preventDefault();
-        const formData = new FormData(e.target);
-        const formProps = Object.fromEntries(formData);
-        dispatchForm({ type: 'SUBMIT', payload: formProps });
+        dispatchForm({ type: 'SUBMIT' });
     };
 
     return (
         <form className={`${styles['journal-form']}`} onSubmit={addJournalItem}>
             <div>
-                <input type="text" name='title' className={cn(styles['input'], styles['input-title'], {
-                    [styles['invalid']]: !isValid.title
-                })} />
+                <input type="text" name='title' value={values.title} onChange={onChange}
+                    className={cn(styles['input'], styles['input-title'], {
+                        [styles['invalid']]: !isValid.title
+                    })} />
             </div>
             <div className={styles['form-row']}>
                 <label htmlFor='date' className={styles['form-label']}>
                     <img src="/calendary.svg" alt="Иконка календаря" />
                     <span>Дата</span>
                 </label>
-                <input type="date" name='date'
+                <input type="date" name='date' value={values.date} onChange={onChange}
                     className={cn(styles['input'], {
                         [styles['invalid']]: !isValid.date
                     })}
@@ -56,11 +60,11 @@ function JournalForm({ onSubmit }) {
                     <img src="/folder.svg" alt="Иконка папки" />
                     <span>Метка</span>
                 </label>
-                <input type="text" name='tag'
+                <input type="text" name='tag' value={values.tag} onChange={onChange}
                     className={`${styles['input']}`}
                     id='tag' />
             </div>
-            <textarea name="text" id="" cols={32} rows={18}
+            <textarea name="text" id="" cols={32} rows={18} value={values.text} onChange={onChange}
                 className={cn(styles['input'], {
                     [styles['invalid']]: !isValid.text
                 })}>
--- react-app/src/components/JournalForm/JournalForm.state.js
+++ react-app/src/components/JournalForm/JournalForm.state.js
@@ -5,23 +5,31 @@ export const INITIAL_STATE = {
         date: true
     },
     values: {
-        text: undefined,
-        title: undefined,
-        date: undefined
+        text: '',
+        title: '',
+        date: '',
+        tag: ''
     },
     isFormReadyToSubmit: false
 };
 
 export function formReducer(state, action) {
     switch (action.type) {
+        case 'SET_VALUE':
+            return { ...state, values: { ...state.values, ...action.payload } };
+
+        case 'CLEAR':
+            return { ...state, values: INITIAL_STATE.values };
+
         case 'RESET_VALIDITY':
             return { ...state, isValid: INITIAL_STATE.isValid };
+
         case 'SUBMIT': {
-            const titleValidity = action.payload.title?.length;
-            const textValidity = action.payload.text?.length;
-            const dateValidity = action.payload.date;
+            const titleValidity = state.values.title?.length;
+            const textValidity = state.values.text?.length;
+            const dateValidity = state.values.date;
             return {
-                values: action.payload,
+                ...state,
                 isValid: {
                     text: textValidity,
                     title: titleValidity,
@@ -29,7 +37,6 @@ export function formReducer(state, action) {
                 },
                 isFormReadyToSubmit: titleValidity && textValidity && dateValidity
             };
-
         }
     }
 }
