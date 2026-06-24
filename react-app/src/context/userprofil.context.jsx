7228407b358623b8e34fb1e5528a0036a48f1eb6
The Great White Rabbit <gribomen@mail.ru>
2026-06-24 22:17:46 +0300

    Подправил коммит

--- react-app/src/context/userprofil.context.jsx
+++ react-app/src/context/userprofil.context.jsx
@@ -3,7 +3,19 @@ import { createContext, useState } from 'react';
 export const UserProfile = createContext({ name: '', isLogined: false });
 
 export function UserProfileProvider({ children }) {
-    const [profile, setProfile] = useState({ name: '', isLogined: false });
+    const [profile, setProfile] = useState(() => {
+        try {
+            const storage = JSON.parse(localStorage.getItem('profiles'));
+            for (let item of storage) {
+                if (item.isLogined) {
+                    return item;
+                }
+            }
+            return { name: '', isLogined: false };
+        } catch (error) {
+            console.error('Ошибка чтения localStorage ключа "profils": ', error);
+        }
+    });
     return (
         <UserProfile.Provider value={{ profile, setProfile }}>
             {children}
