87a9908cd2b4476923146373368893c146b73560
The Great White Rabbit <gribomen@mail.ru>
2026-06-30 21:32:22 +0300

    Добавил Input и Button

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, type MouseEvent } from 'react';
 import Button from './components/Button/Button';
+import Input from './components/Input/Input';
 
 function App() {
 	const [counter, setCounter] = useState<number>();
@@ -11,6 +12,8 @@ function App() {
 	return (
 		<>
 			<Button onClick={addCounter} >Кнопка</Button>
+			<Button appearence='big' onClick={addCounter} >Кнопка</Button>
+			<Input placeholder='Email' />
 		</>
 	);
 }
