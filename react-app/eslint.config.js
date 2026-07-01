ea43b206a69d33a7f0eaebdaf1b2afd83a1b0561
The Great White Rabbit <gribomen@mail.ru>
2026-07-01 11:51:08 +0300

    Мигрировал на ts

--- react-app/eslint.config.js
+++ react-app/eslint.config.js
@@ -2,31 +2,32 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaVersion: 'latest', ecmaFeatures: { jsx: true } }
-    },
-    rules: {
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
-      'semi': ['error', 'always', { 'omitLastInOneLineBlock': false }],
-      'comma-dangle': ['error', 'never'],
-      quotes: ['error', 'single'],
-      'react/prop-types': [0]
-      //'indent': ['error', 'tab']
-    }
-  }
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite
+		],
+		languageOptions: {
+			globals: globals.browser
+		},
+		rules: {
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true }
+			],
+			'semi': ['error', 'always', { 'omitLastInOneLineBlock': false }],
+			'comma-dangle': ['error', 'never'],
+			quotes: ['error', 'single'],
+			'react/prop-types': [0],
+			'indent': ['error', 'tab']
+		}
+	}
 ]);
