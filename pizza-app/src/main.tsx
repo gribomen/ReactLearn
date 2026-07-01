54dc3cde0435f8e5501311023764bca2ef972302
The Great White Rabbit <gribomen@mail.ru>
2026-07-01 23:56:03 +0300

    Познакомился с Outlet

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -5,15 +5,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Menu } from './pages/Menu/Menu.tsx';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error } from './pages/Error/Error.tsx';
+import { Layout } from './layout/Menu/Menu.tsx';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Menu />
-	},
-	{
-		path: '/cart',
-		element: <Cart />
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Menu />
+			},
+			{
+				path: '/cart',
+				element: <Cart />
+			}
+		]
 	},
 	{
 		path: '*',
