3eb77b19fd86947675701538ab7bd34677fa7745
The Great White Rabbit <gribomen@mail.ru>
2026-07-02 15:58:00 +0300

    Изучил работу нового компонента NavLink

--- pizza-app/src/layout/Layout/Layout.tsx
+++ pizza-app/src/layout/Layout/Layout.tsx
@@ -1,16 +1,9 @@
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
-import { useEffect } from 'react';
 import cn from 'classnames';
 
 export function Layout() {
-	const location = useLocation();
-
-	useEffect(() => {
-		console.log(location);
-	}, [location]);
-
 	return <div className={styles['layout']}>
 		<div className={styles['sidebar']}>
 			<div className={styles['user']}>
@@ -19,16 +12,18 @@ export function Layout() {
 				<div className={styles['email']}>alaricode@ya.ru</div>
 			</div>
 			<div className={styles['menu']}>
-				<Link to="/" className={styles['link']}>
+				<NavLink to="/" className={({ isActive }) => cn(styles['link'], {
+					[styles['active']]: isActive
+				})}>
 					<img src="/menu-icon.svg" alt="Иконка меню" />
 					Меню
-				</Link>
-				<Link to="/cart" className={cn(styles['link'], {
-					[styles['active']]: location.pathname == '/cart'
+				</NavLink>
+				<NavLink to="/cart" className={({ isActive }) => cn(styles['link'], {
+					[styles['active']]: isActive
 				})}>
 					<img src="/cart-icon.svg" alt="Иконка корзины" />
 					Корзина
-				</Link>
+				</NavLink>
 			</div>
 			<Button className={styles['exit']}>
 				<img src="/exit-icon.svg" alt="Иконка выхода" />
