535190fffc8bf93d93a736e44e90b3d613116d15
Гриб Вячеслав Олегович <GribVO@hq.root>
2026-07-07 22:44:34 +0300

    подтянул данные с бека

--- pizza-app/src/components/ProductCard/ProductCard.tsx
+++ pizza-app/src/components/ProductCard/ProductCard.tsx
@@ -21,7 +21,7 @@ const ProductCard: FC<ProductCardProps> = (props) => {
 					</div>
 				</div>
 				<div className={styles['footer']}>
-					<div className={styles['title']}>{props.title}</div>
+					<div className={styles['title']}>{props.name}</div>
 					<div className={styles['description']}>{props.description}</div>
 				</div>
 			</div>
