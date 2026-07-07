af0aa2bb0cc4fc0ece5ce6896cef00f61cd35b17
Гриб Вячеслав Олегович <GribVO@hq.root>
2026-07-07 22:51:38 +0300

    перенес на axios

--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -5,23 +5,19 @@ import Search from '../../components/Search/Search';
 import { PREFIX } from '../../helpers/API';
 import type { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
+import axios from 'axios';
 
 export function Menu() {
 	const [products,setProducts] = useState<Product[]>([]);
 
 	const getMenu = async () => {
 		try{
-			const res = await fetch(`${PREFIX}products`);
-			if(!res.ok){
-				return;
-			}
-			const data = await res.json() as Product[];
+			const {data} = await axios.get<Product[]>(`${PREFIX}products`);
 			setProducts(data);
 		}catch(e){
 			console.error(e);
 			return;
 		}
-
 	};
 
 	useEffect(() => {
